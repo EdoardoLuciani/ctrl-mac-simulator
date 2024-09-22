b92dcf421af17c593f11ad863a5007ef5c16b607
EdoardoLuciani <edoardo.luciani@gmail.com>
2024-09-22 15:25:02 +0100

    Remove not needed parenthesis

--- src/ctrl_mac_simulator/messages/request_reply_message.py
+++ src/ctrl_mac_simulator/messages/request_reply_message.py
@@ -1,15 +1,14 @@
 from typing import TypedDict, Literal
 from dataclasses import dataclass
 
+
 @dataclass
-class _RequestSlot():
+class _RequestSlot:
     state: Literal["free", "no_collision", "collision_occurred"]
     data_slot: int
     data_channel: int
 
 
-class RequestReplyMessage():
+class RequestReplyMessage:
     def __init__(self, request_slots: int = 5):
-        self.message = [
-            _RequestSlot("free", 0, 0) for i in range(request_slots)
-        ]
+        self.message = [_RequestSlot("free", 0, 0) for i in range(request_slots)]
