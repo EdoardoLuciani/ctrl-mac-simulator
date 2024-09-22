dc09d6b925531a17a00e996e3e6458b4e1ced42f
EdoardoLuciani <edoardo.luciani@gmail.com>
2024-09-22 18:12:22 +0100

    Add logging level parsing

--- src/ctrl_mac_simulator/main.py
+++ src/ctrl_mac_simulator/main.py
@@ -1,10 +1,15 @@
-import simpy, random, logging
+import simpy, random, logging, argparse
 
 from ctrl_mac_simulator.messages import RequestReplyMessage
 from ctrl_mac_simulator.airtime import get_lora_airtime
 from ctrl_mac_simulator.devices import Sensor, Actuator, Gateway
 
-logging.basicConfig(level=logging.INFO)
+parser = argparse.ArgumentParser(description='Simulate the Ctrl-Mac protocol')
+parser.add_argument('--log', dest='loglevel', default='INFO', help='Set the log level')
+args = parser.parse_args()
+
+logging.basicConfig(level=getattr(logging, args.loglevel.upper()))
+
 
 # Set up and run the simulation
 env = simpy.Environment()
