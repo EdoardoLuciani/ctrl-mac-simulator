00b22e1cad3e6219edbfb5cd5c323cd309d92aa4
EdoardoLuciani <edoardo.luciani@gmail.com>
2024-09-23 18:34:29 +0100

    Fix airtime test

--- tests/test_airtime.py
+++ tests/test_airtime.py
@@ -1,10 +1,9 @@
 from ctrl_mac_simulator.messages.sensor_measurement_message import SensorMeasurementMessage
 import pytest
-from src.ctrl_mac_simulator.airtime import get_lora_airtime
 
 
 def test_airtime():
     # Reference value computed by https://www.loratools.nl/#/airtime
     sensor_message = SensorMeasurementMessage(0, 0, 12)
 
-    assert get_lora_airtime(25, True, False) == pytest.approx(0.05658, 1e-4)
+    assert sensor_message.get_airtime() == pytest.approx(0.04122, 1e-4)
