87b6b33079dfa190159b58abfec5f8d1f5bbe850
EdoardoLuciani <edoardo.luciani@gmail.com>
2024-09-27 00:31:35 +0100

    Rename fields in sensor

--- src/ctrl_mac_simulator/devices/sensor.py
+++ src/ctrl_mac_simulator/devices/sensor.py
@@ -17,23 +17,22 @@ class Sensor:
         get_rrm_message_event_fn: Callable[[], simpy.Event],
         sensor_messages_queue: simpy.Store,
     ):
-        self.env = env
-        self.id = id
-        self.measurement_chance = measurement_chance
-        self.get_rrm_message_event_fn = get_rrm_message_event_fn
-        self.sensor_messages_queue = sensor_messages_queue
+        self._env = env
+        self._id = id
+        self._measurement_chance = measurement_chance
+        self._get_rrm_message_event_fn = get_rrm_message_event_fn
+        self._sensor_messages_queue = sensor_messages_queue
 
-        self.logger = logging.getLogger(self.__class__.__name__ + f"-{id}")
-        self.env.process(self.run())
+        self._logger = logging.getLogger(self.__class__.__name__ + f"-{id}")
+        self._env.process(self.run())
 
         self.transition_to(_IdleState())
 
     def run(self):
         while True:
             # Get the RRM message
-            event = self.get_rrm_message_event_fn()
-            rrm_message: RequestReplyMessage = yield event
-            self.logger.info(f"Time {self.env.now:.2f}: Received RRM message")
+            rrm_message = yield self._get_rrm_message_event_fn()
+            self._logger.info(f"Time {self._env.now:.2f}: Received RRM message")
 
             # Proceed with the state action
             yield from self._state.handle(rrm_message)
@@ -43,17 +42,17 @@ class Sensor:
         The Context allows changing the State object at runtime.
         """
         self._state = state
-        self._state.context = self
+        self._state.sensor = self
 
 
 class _State(ABC):
     @property
-    def context(self) -> Sensor:
-        return self._context
+    def sensor(self) -> Sensor:
+        return self._sensor
 
-    @context.setter
-    def context(self, context: Sensor) -> None:
-        self._context = context
+    @sensor.setter
+    def sensor(self, sensor: Sensor) -> None:
+        self._sensor = sensor
 
     @abstractmethod
     def handle(self, rrm_message):
@@ -62,14 +61,14 @@ class _State(ABC):
 
 class _IdleState(_State):
     def handle(self, rrm_message):
-        if random.random() <= self._context.measurement_chance:
-            self.context.logger.info(
-                f"Time {self.context.env.now:.2f}: Data is available, syncing to next RRM for transmission request"
+        if random.random() <= self.sensor._measurement_chance:
+            self.sensor._logger.info(
+                f"Time {self.sensor._env.now:.2f}: Data is available, syncing to next RRM for transmission request"
             )
-            self.context.transition_to(_TransmissionRequestState())
+            self.sensor.transition_to(_TransmissionRequestState())
         else:
-            self.context.logger.info(
-                f"Time {self.context.env.now:.2f}: No data available, skipping transmission request"
+            self.sensor._logger.info(
+                f"Time {self.sensor._env.now:.2f}: No data available, skipping transmission request"
             )
         yield from ()
 
@@ -82,13 +81,13 @@ class _TransmissionRequestState(_State):
         free_request_slot_idx = rrm_message.sample_free_request_slot()
 
         if free_request_slot_idx != None:
-            message = TransmissionRequestMessage(self.context.id, free_request_slot_idx, self.context.env.now)
+            message = TransmissionRequestMessage(self.sensor._id, free_request_slot_idx, self.sensor._env.now)
 
-            yield self.context.env.process(message.send_message(self.context.env, self.context.logger))
-            yield self.context.sensor_messages_queue.put(message)
+            yield self.sensor._env.process(message.send_message(self.sensor._env, self.sensor._logger))
+            yield self.sensor._sensor_messages_queue.put(message)
 
-            self.context.transition_to(_IdleState())
+            self.sensor.transition_to(_IdleState())
         else:
-            self.context.logger.info(
-                f"Time {self.context.env.now:.2f}: No available free slot to choose, skipping transmission request"
+            self.sensor._logger.info(
+                f"Time {self.sensor._env.now:.2f}: No available free slot to choose, skipping transmission request"
             )
