d2f83d7df675eac39b3e1190e15b1f9f920d22fa
EdoardoLuciani <edoardo.luciani@gmail.com>
2024-09-27 17:55:31 +0100

    Implement logic for ftr computation and backoff

--- src/ctrl_mac_simulator/messages/request_reply_message.py
+++ src/ctrl_mac_simulator/messages/request_reply_message.py
@@ -1,5 +1,5 @@
 import math, json, random
-from typing import TypedDict, Literal
+from typing import TypedDict, Literal, Optional
 from dataclasses import dataclass
 
 from ctrl_mac_simulator.messages.abstract_message import AbstractMessage
@@ -25,7 +25,11 @@ class RequestReplyMessage(AbstractMessage):
                 slot += data_slot_time_offset
 
         self.ftr = 0
+        self.old_ftr = 0
+        self.old_contentions = 0
+
         self._start_time = start_time
+        self._contentions = 0
 
     @property
     def start_time(self) -> float:
@@ -41,11 +45,17 @@ class RequestReplyMessage(AbstractMessage):
         # 4 bits for the ftr
         return math.ceil(((2 + 4 + 2) * len(self.request_slots) + 4) / 8)
 
-    def sample_free_request_slot(self) -> int | None:
-        free_slots = [slot for slot in self.request_slots if slot.state == "free"]
-        if free_slots:
-            return self.request_slots.index(random.choice(free_slots))
-        return None
+    def sample_free_request_slot(self) -> Optional[int]:
+        free_slot_indices = [
+            index for index, slot in enumerate(self.request_slots)
+            if slot.state == "free"
+        ]
+        return random.choice(free_slot_indices) if free_slot_indices else None
 
     def update_ftr(self) -> None:
-        self.ftr += len([slot for slot in self.request_slots if slot.state == "contention_occurred"])
+        self.ftr = max(0, self.old_ftr + self.old_contentions - 1)
+        self.old_ftr, self.old_contentions = self.ftr, self.total_contentions()
+
+    def total_contentions(self, before_slot: int | None = None) -> int:
+            end_slot = before_slot if before_slot is not None else len(self.request_slots)
+            return sum(1 for slot in self.request_slots[:end_slot] if slot.state == "contention_occurred")
