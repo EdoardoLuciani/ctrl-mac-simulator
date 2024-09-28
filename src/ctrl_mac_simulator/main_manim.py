d7d1e9b129807128e0351a6c18438abd5279d1e8
EdoardoLuciani <edoardo.luciani@gmail.com>
2024-09-28 17:54:13 +0100

    Restructure code

--- src/ctrl_mac_simulator/main_manim.py
+++ src/ctrl_mac_simulator/main_manim.py
@@ -1,11 +1,10 @@
-from ctrl_mac_simulator.devices import gateway
 from typing import Tuple
 from manim import *
 
 class CreateCircle(Scene):
     def setup_scene(self, num_sensors, sensor_radius) -> Tuple[Circle, VGroup]:
         # Create gateway
-        gateway = Circle(radius=0.25, color=BLUE)
+        gateway = Circle(radius=0.3, color=BLUE)
         gateway.to_edge(ORIGIN)
 
         gateway_label = Text("Gateway", font_size=16).next_to(gateway, DOWN)
@@ -19,27 +18,21 @@ class CreateCircle(Scene):
         sensor_labels = VGroup()
 
         for i in range(num_sensors):
-            sensor = Circle(radius=0.15, color=RED)
+            sensor = Circle(radius=0.2, color=RED)
             sensor.move_to(sensor_radius * np.array([np.cos(i * angle), np.sin(i * angle), 0]))
             sensors.add(sensor)
 
-            label = Text(f"S{i}", font_size=16).next_to(sensor, (sensor.get_center() - ORIGIN)*0.2)
+            label = Text(f"S{i}", font_size=16, color=RED).move_to(sensor.get_center())
             sensor_labels.add(label)
 
-        self.play(Create(sensors), Write(sensor_labels))
+        self.play(Create(sensors), FadeIn(sensor_labels))
 
         return gateway, sensors
 
 
-    def construct(self):
-        sensor_radius = 3.5
-        num_sensors = 5
-
-        gateway, sensors = self.setup_scene(num_sensors, sensor_radius)
-
-        # Add expanding circle animation
+    def display_rrm(self, sensor_radius):
         expanding_circle = Circle(radius=0)
-        expanding_circle.move_to(gateway.get_center())
+        expanding_circle.move_to(self._gateway.get_center())
 
         def update_circle(c, alpha):
             c.become(Circle(radius=sensor_radius * alpha, color=YELLOW, stroke_opacity=0.5))
@@ -47,10 +40,19 @@ class CreateCircle(Scene):
         self.play(UpdateFromAlphaFunc(expanding_circle, update_circle, run_time=1))
         self.play(FadeOut(expanding_circle))
 
+
+    def construct(self):
+        sensor_radius = 4
+
+        self._gateway, self._sensors = self.setup_scene(5, sensor_radius)
+
+        self.display_rrm(sensor_radius)
+
+
         # Transmission request animation
-        for i in range(num_sensors):
+        for i in range(len(self._sensors)):
             if i % 2 == 0:  # Only some sensors send requests
-                req_arrow = Arrow(sensors[i].get_center(), gateway.get_center(), buff=0.3, color=YELLOW)
+                req_arrow = Arrow(self._sensors[i].get_center(), self._gateway.get_center(), buff=0.3, color=YELLOW)
                 req_label = Text("Request", font_size=16, color=YELLOW).next_to(req_arrow, LEFT)
                 self.play(GrowArrow(req_arrow), Write(req_label))
                 self.wait(0.5)
