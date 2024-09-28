49201d069834ca2f860065d8413174b4abf1fb61
EdoardoLuciani <edoardo.luciani@gmail.com>
2024-09-28 03:01:26 +0100

    Add manim mai

---
+++ src/ctrl_mac_simulator/main_manim.py
@@ -0,0 +1,23 @@
+from manim import *
+
+
+class CreateCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set color and transparency
+
+        square = Square()  # create a square
+        square.rotate(PI / 4)  # rotate a certain amount
+
+        self.play(Create(square))  # animate the creation of the square
+        self.play(Transform(square, circle))  # interpolate the square into the circle
+        self.play(FadeOut(square))  # fade out animation
+
+
+if __name__ == "__main__":
+    config.frame_rate = 15
+    config.pixel_height = 720
+    config.pixel_width = 1280
+
+    scene = CreateCircle()
+    scene.render(preview=True)
