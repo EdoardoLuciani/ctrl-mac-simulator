922639542922111d96f7e9e75f610a31705fcc43
EdoardoLuciani <edoardo.luciani@gmail.com>
2024-09-29 19:41:41 +0100

    Allow to add rows to the rrm table

--- src/visual/components/left_sidebar.py
+++ src/visual/components/left_sidebar.py
@@ -3,18 +3,29 @@ from manim import *
 
 class _RRMTable:
     def __init__(self, request_slots: int, down_from: Mobject):
-        # Create the table for keeping track of the events
-        self._table = (
-            Table([["FTR"] + [f"Slot {i}" for i in range(request_slots)], ["dunno" for i in range(request_slots + 1)]])
-            .scale(0.3)
-            .next_to(down_from, DOWN)
-            .to_edge(LEFT)
-        )
-        self._table.add_highlighted_cell((2, 2), color=GREEN)
+        self._down_from = down_from
+        self._array = [
+            [f"Slot {i}" for i in range(request_slots)] + ["FTR"]
+        ]
+        self._table = self._get_table()
 
     @property
     def object(self):
         return self._table
+
+    def add_row(self, new_row: List, scene):
+        self._array.append(new_row)
+        scene.play(Transform(self.object, self._get_table()), run_time=0.1)
+
+    def _get_table(self):
+        table = (
+            Table(self._array, h_buff=0.5)
+            .scale(0.2)
+            .next_to(self._down_from, DOWN)
+            .to_edge(LEFT)
+        )
+        table.add_highlighted_cell((1, 1), color=GREEN)
+        return table
 
 
 class _Timer:
@@ -29,22 +40,25 @@ class _Timer:
 
     def update_timer(self, dt, scene):
         self._time += dt
-        scene.play(Transform(self._text, self._get_timer_text()), run_time=0.1)
+        scene.play(Transform(self.object, self._get_timer_text()), run_time=0.1)
 
     def _get_timer_text(self):
         return Text(f"Time: {self._time:.2f}s", font_size=24).to_corner(UL)
 
 
 class LeftSidebar:
-    def __init__(self, scene):
+    def __init__(self, scene, request_slots):
         self._scene = scene
 
         # Create timer
         self._timer = _Timer()
 
-        self._rrm_table = _RRMTable(3, self._timer._text)
+        self._rrm_table = _RRMTable(request_slots, self._timer._text)
 
         self._scene.play(FadeIn(self._timer.object), FadeIn(self._rrm_table.object))
 
     def update_timer(self, dt):
         self._timer.update_timer(dt, self._scene)
+
+    def add_row(self, new_row):
+        self._rrm_table.add_row(new_row, self._scene)
