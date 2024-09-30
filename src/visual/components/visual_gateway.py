e0455648bdf59b19fb6a7c890a5be10c14a9f872
EdoardoLuciani <edoardo.luciani@gmail.com>
2024-09-30 15:51:58 +0100

    Abstract gateway component

---
+++ src/visual/components/visual_gateway.py
@@ -0,0 +1,21 @@
+from manim import *
+
+class VisualGateway():
+    def __init__(self, scene: Scene, gateway_position):
+        self._scene = scene
+
+        self._gateway = Circle(radius=0.3, color=BLUE).move_to(gateway_position)
+        gateway_label = Text("Gateway", font_size=16).next_to(self._gateway, DOWN)
+
+        scene.play(Create(self._gateway), Write(gateway_label))
+
+    def display_rrm(self, sensor_radius):
+        expanding_circle = Circle(radius=sensor_radius, color=YELLOW, stroke_opacity=0.5).move_to(
+            self._gateway.get_center()
+        )
+
+        text = Text(f"RRM", color=YELLOW, font_size=24).move_to(self._gateway.get_center())
+        text_dst = text.copy().move_to((UL / np.linalg.norm(UL)))
+
+        self._scene.play(GrowFromCenter(expanding_circle, run_time=1), Transform(text, text_dst, run_time=1))
+        self._scene.play(FadeOut(expanding_circle), FadeOut(text))
