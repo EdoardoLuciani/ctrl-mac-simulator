43fa9704ba8b57b6e84455bd9983ef43b449707c
EdoardoLuciani <edoardo.luciani@gmail.com>
2024-10-05 00:33:35 +0100

    Add and feed parameters

--- src/ctrl_mac_simulator/visual/main_manim.py
+++ src/ctrl_mac_simulator/visual/main_manim.py
@@ -6,23 +6,25 @@ from ctrl_mac_simulator.visual.components.visual_sensor import VisualSensors
 
 
 class ManimMainScene(Scene):
+    def set_params(self, sensor_count, request_slots):
+        self.sensor_count = sensor_count
+        self.request_slots = request_slots
+
     def construct(self):
         gateway_center = np.array((3, 0, 0))
-        sensors_count = 5
         sensors_arrangement_radius = 3.8
-        request_slots = 7
 
         self._gateway = VisualGateway(self, gateway_center)
-        self._sensors = VisualSensors(self, sensors_count, sensors_arrangement_radius, self._gateway.object)
-        self._left_sidebar = LeftSidebar(self, request_slots)
+        self._sensors = VisualSensors(self, self.sensor_count, sensors_arrangement_radius, self._gateway.object)
+        self._left_sidebar = LeftSidebar(self, self.request_slots)
 
         self._gateway.display_rrm(sensors_arrangement_radius)
 
         self._left_sidebar.update_timer(1)
-        self._left_sidebar.add_row(list(map(str, range(request_slots + 1))))
+        self._left_sidebar.add_row(list(map(str, range(self.request_slots + 1))))
 
         # Transmission request animation
-        for i in range(sensors_count):
+        for i in range(self.sensor_count):
             if i % 2 == 0:
                 self._sensors.display_transmission_request_message(i)
                 self._sensors.display_data_transmission(i)
