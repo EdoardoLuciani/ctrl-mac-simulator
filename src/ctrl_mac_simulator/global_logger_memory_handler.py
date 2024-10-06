0bde0790c45d16a909a4c84640ba2666cca9b1b3
EdoardoLuciani <edoardo.luciani@gmail.com>
2024-10-06 13:41:19 +0100

    Allow function to return multiple matches per logger

--- src/ctrl_mac_simulator/global_logger_memory_handler.py
+++ src/ctrl_mac_simulator/global_logger_memory_handler.py
@@ -10,9 +10,6 @@ class GlobalLoggerMemoryHandler(logging.Handler):
     def emit(self, record: logging.LogRecord):
         self.logs.append(f"{record.name}: {(record.msg)}")
 
-    def match_event_in_sublist(self, pattern: str, starting_idx: int) -> Optional[str]:
-        for item in self.logs[starting_idx:]:
-            match = re.search(f"([^:]+): Time [0-9]*.[0-9]*: {pattern}", item)
-            if match:
-                return match.group(1)
-        return None
+    def match_events_in_sublist(self, pattern: str, starting_idx: int) -> Optional[str]:
+        matches = list(map(lambda x: x.group(1), filter(lambda x: x is not None, [re.search(f"([^:]+): Time [0-9]*.[0-9]*: {pattern}", item) for item in self.logs[starting_idx:]])))
+        return matches if matches else None
--- tests/test_global_logger_memory_handler.py
+++ tests/test_global_logger_memory_handler.py
@@ -8,10 +8,11 @@ def test_match_event(caplog):
     logging.basicConfig(level=logging.INFO, handlers=[glmh])
 
     logging.info("Time 0.00: Something has happened")
+    logging.info("Time 0.01: Something has happened")
 
     alice_logger = logging.getLogger('Alice')
     alice_logger.info("Time 0.00: Something else has happened")
 
-    assert glmh.match_event_in_sublist("Something has happened", 0) == 'root'
-    assert glmh.match_event_in_sublist("Something else has happened", 0) == 'Alice'
-    assert glmh.match_event_in_sublist("Something that has not happened", 0) is None
+    assert glmh.match_events_in_sublist("Something has happened", 0) == ['root', 'root']
+    assert glmh.match_events_in_sublist("Something else has happened", 0) == ['Alice']
+    assert glmh.match_events_in_sublist("Something that has not happened", 0) is None
