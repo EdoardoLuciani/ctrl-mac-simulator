586d3389cbe3929c1ef59a7a96e96671b9a8d83f
EdoardoLuciani <edoardo.luciani@gmail.com>
2024-10-06 13:22:02 +0100

    Add test for the logger handler

--- src/ctrl_mac_simulator/global_logger_memory_handler.py
+++ src/ctrl_mac_simulator/global_logger_memory_handler.py
@@ -1,5 +1,5 @@
 import logging, re
-from typing import List, Pattern
+from typing import List, Pattern, Optional
 
 
 class GlobalLoggerMemoryHandler(logging.Handler):
@@ -7,8 +7,12 @@ class GlobalLoggerMemoryHandler(logging.Handler):
         super().__init__()
         self.logs = []
 
-    def emit(self, record):
-        self.logs.append(self.format(record))
+    def emit(self, record: logging.LogRecord):
+        self.logs.append(f"{record.name}: {(record.msg)}")
 
-    def match_event_in_sublist(self, pattern: str, starting_idx: int) -> int:
-        return any(re.search(f"Time [0-9]*.[0-9]*: {pattern}", item) for item in self.logs[starting_idx:])
+    def match_event_in_sublist(self, pattern: str, starting_idx: int) -> Optional[str]:
+        for item in self.logs[starting_idx:]:
+            match = re.search(f"([^:]+): Time [0-9]*.[0-9]*: {pattern}", item)
+            if match:
+                return match.group(1)
+        return None
---
+++ tests/test_global_logger_memory_handler.py
@@ -0,0 +1,17 @@
+import logging
+
+from ctrl_mac_simulator.global_logger_memory_handler import GlobalLoggerMemoryHandler
+
+
+def test_match_event(caplog):
+    glmh = GlobalLoggerMemoryHandler()
+    logging.basicConfig(level=logging.INFO, handlers=[glmh])
+
+    logging.info("Time 0.00: Something has happened")
+
+    alice_logger = logging.getLogger('Alice')
+    alice_logger.info("Time 0.00: Something else has happened")
+
+    assert glmh.match_event_in_sublist("Something has happened", 0) == 'root'
+    assert glmh.match_event_in_sublist("Something else has happened", 0) == 'Alice'
+    assert glmh.match_event_in_sublist("Something that has not happened", 0) is None
