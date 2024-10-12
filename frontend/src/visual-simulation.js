d7a438585f18b95d7e671a7685d0fdaf81fb9d8c
EdoardoLuciani <edoardo.luciani@gmail.com>
2024-10-12 17:55:26 +0100

    Split visualisation vertically using flexbox

--- frontend/src/visual-simulation.js
+++ frontend/src/visual-simulation.js
@@ -1,8 +1,24 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
 export function setupCanvas() {
-  const canvas = document.getElementById("canvas");
-  const ctx = canvas.getContext("2d");
+  clearCanvas();
 
   ctx.beginPath();
   ctx.arc(0, 0, 40, 0, 2 * Math.PI);
   ctx.stroke();
+}
+
+export function clearCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+export function resizeCanvas() {
+  const displayWidth = canvas.clientWidth;
+  const displayHeight = canvas.clientHeight;
+
+  if (canvas.width !== displayWidth || canvas.height !== displayHeight) {
+    canvas.width = displayWidth;
+    canvas.height = displayHeight;
+  }
 }
