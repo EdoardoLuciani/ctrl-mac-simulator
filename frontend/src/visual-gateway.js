dc1e296323c478e61d05d77e1b4efb9890e3ad21
EdoardoLuciani <edoardo.luciani@gmail.com>
2024-10-13 16:32:38 +0100

    Add text in gateway

--- frontend/src/visual-gateway.js
+++ frontend/src/visual-gateway.js
@@ -1,12 +1,28 @@
 export class VisualGateway {
   constructor(x, y) {
-    this.gateway = new Konva.Circle({
+    this.gateway = new Konva.Group({
       x: x,
       y: y,
-      radius: 70,
-      stroke: "blue",
-      strokeWidth: 4,
     });
+
+    this.gateway.add(
+      new Konva.Circle({
+        radius: 70,
+        stroke: "blue",
+        strokeWidth: 4,
+      }),
+    );
+
+    const text = new Konva.Text({
+      x: 0,
+      y: 0,
+      text: "Gateway",
+      fontSize: 18,
+      fontFamily: "Arial",
+    });
+    text.x(-text.width() / 2);
+
+    this.gateway.add(text);
   }
 
   get shape() {
