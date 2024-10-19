c692dd24d56f1347266003630bfbb1fd83168ad4
EdoardoLuciani <edoardo.luciani@gmail.com>
2024-10-19 17:40:42 +0100

    Add function for highlighting text

--- frontend/src/log-highlighter.js
+++ frontend/src/log-highlighter.js
@@ -8,4 +8,19 @@ export class LogHighligther {
   set text(x) {
     document.getElementById("result").textContent = x;
   }
+
+  highlightLines(startLine, endLine) {
+    const lines = this.text.split("\n");
+
+    // Highlight the specified lines
+    for (
+      let i = Math.max(startLine - 1, 0);
+      i < Math.min(endLine, lines.length);
+      i++
+    ) {
+      lines[i] = `<mark>${lines[i]}</mark>`;
+    }
+
+    document.getElementById("result").innerHTML = lines.join("\n");
+  }
 }
