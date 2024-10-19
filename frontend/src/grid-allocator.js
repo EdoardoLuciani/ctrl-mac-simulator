cf51c49aa944a31e5cd377a9467914dda22aef0a
EdoardoLuciani <edoardo.luciani@gmail.com>
2024-10-19 02:03:37 +0100

    Allocate the sensors via grid

---
+++ frontend/src/grid-allocator.js
@@ -0,0 +1,39 @@
+export class GridAllocator {
+  constructor(x, y, elemSize, maxElemsPerRow, maxElems) {
+    this.allocations = new Array(Number(maxElems)).fill(null);
+
+    console.log(this.allocations);
+
+    this.elemSize = elemSize;
+    this.maxElemsPerRow = maxElemsPerRow;
+    this.x = x;
+    this.y = y;
+  }
+
+  allocate(handle) {
+    const index = this.allocations.findIndex((element) => element === null);
+    if (index === -1) {
+      console.error("Allocator is full");
+      return null;
+    }
+
+    this.allocations[index] = handle;
+    return this.#getXYPosFromIndex(index);
+  }
+
+  free(handle) {
+    const index = this.allocations.findIndex((element) => element === handle);
+    if (index === -1) {
+      console.error("Could not find provided handle");
+      return null;
+    }
+    this.allocations[index] = null;
+  }
+
+  #getXYPosFromIndex(index) {
+    return {
+      x: this.x + (index % this.maxElemsPerRow) * this.elemSize,
+      y: this.y + Math.floor(index / this.maxElemsPerRow) * this.elemSize,
+    };
+  }
+}
