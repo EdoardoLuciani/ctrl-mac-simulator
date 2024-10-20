d5d7daaf84604794dc64d74689c531bb759c192a
EdoardoLuciani <edoardo.luciani@gmail.com>
2024-10-20 01:39:42 +0100

    Cleanup code

--- frontend/src/plotter.js
+++ frontend/src/plotter.js
@@ -9,68 +9,48 @@ export class Plotter {
     const trace1 = {
       x: Array.from(Array(ftrValues.length).keys()),
       y: ftrValues,
-      mode: "lines+markers",
-      name: "FTR Time Series",
       type: "scatter",
+      mode: "lines+markers",
+      name: "FTR Values",
     };
 
-    // const trace2 = {
-    //   x: measurementLatencies,
-    //   type: "histogram",
-    //   nbinsx: 20,
-    //   name: "Measurement Latency",
-    // };
     var trace2 = {
       x: measurementLatencies,
+      type: "histogram",
       xaxis: "x2",
       yaxis: "y2",
-      type: "histogram",
-      name: "Measurement Latency",
+      name: "Measurement Latencies",
     };
 
     const layout = {
       grid: { rows: 2, columns: 1, pattern: "independent" },
-      height: 2000,
+      height: 800,
       title: "FTR and Measurement Latency Analysis",
-      template: "plotly_white",
-      // annotations: [
-      //   {
-      //     text: "Discrete Values Time Series",
-      //     xref: "paper",
-      //     yref: "paper",
-      //     x: 0.5,
-      //     y: 1.0,
-      //     xanchor: "center",
-      //     yanchor: "bottom",
-      //     showarrow: false,
-      //   },
-      //   {
-      //     text: "Distribution of Measurement Latency",
-      //     xref: "paper",
-      //     yref: "paper",
-      //     x: 0.5,
-      //     y: 0.45,
-      //     xanchor: "center",
-      //     yanchor: "bottom",
-      //     showarrow: false,
-      //   },
-      // ],
+      annotations: [
+        {
+          text: "FTR Values Time Series",
+          xref: "paper",
+          yref: "paper",
+          x: 0.5,
+          y: 1.0,
+          xanchor: "center",
+          yanchor: "bottom",
+          showarrow: false,
+        },
+        {
+          text: "Distribution of Measurement Latencies",
+          xref: "paper",
+          yref: "paper",
+          x: 0.5,
+          y: 0.45,
+          xanchor: "center",
+          yanchor: "bottom",
+          showarrow: false,
+        },
+      ],
     };
 
     const config = { responsive: true };
-
-    Plotly.react(this.container, [trace1, trace2], layout);
-
-    // Update axis labels
-    // Plotly.update(
-    //   this.container,
-    //   {},
-    //   {
-    //     "xaxis.title": "Array Position",
-    //     "yaxis.title": "Value",
-    //     "xaxis2.title": "Latency",
-    //     "yaxis2.title": "Frequency",
-    //   },
-    // );
+    Plotly.react(this.container, [trace1, trace2], layout, config);
   }
 }
