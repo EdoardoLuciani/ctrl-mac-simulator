22a2ad934c6df786e90e16f27c9e1efbc6081bbd
EdoardoLuciani <edoardo.luciani@gmail.com>
2024-11-09 21:53:09 +0000

    Improve env setup

--- backend/src/ctrl_mac_simulator/main_flask.py
+++ backend/src/ctrl_mac_simulator/main_flask.py
@@ -5,7 +5,6 @@ from http import HTTPStatus
 # Fix for rye that does not load the src directory as a path
 sys.path.insert(0, pathlib.Path(__file__).parents[1].as_posix())
 
-from ctrl_mac_simulator.config import DEFAULT_CONFIG
 from ctrl_mac_simulator.simulation.stat_tracker import StatTracker
 from ctrl_mac_simulator.simulation.devices import Sensor, Gateway
 from ctrl_mac_simulator.global_logger_memory_handler import GlobalLoggerMemoryHandler
@@ -15,12 +14,34 @@ app = Flask(__name__)
 
 @app.route("/api/simulate", methods=["GET"])
 def simulate():
-    # Parse query parameters
-    args = parse_query_params()
+    try:
+        env, stat_tracker, global_logger_memory_handler, gateway, sensors = setup_simulation(**request.args)
+    except ValueError as e:
+        return str(e), HTTPStatus.BAD_REQUEST
+
+    env.run()
+
+    # Prepare response
+    return jsonify({
+        "log": global_logger_memory_handler.log,
+        "ftr_values": stat_tracker.ftr_tracker,
+        "measurement_latencies": stat_tracker.measurement_latencies
+    })
+
+
+def setup_simulation(data_channels: int, data_slots_per_channel: int, sensor_count: int, request_slots: int, rrm_period: float, max_cycles: int, sensors_measurement_chance: float = "1.0", seed: str = None):
+    # Convert string parameters to appropriate numeric types
+    data_channels = int(data_channels)
+    data_slots_per_channel = int(data_slots_per_channel)
+    sensor_count = int(sensor_count)
+    request_slots = int(request_slots)
+    rrm_period = float(rrm_period)
+    max_cycles = int(max_cycles)
+    sensors_measurement_chance = float(sensors_measurement_chance)
 
     # Set seed for deterministic runs
-    if args["seed"] is not None:
-        random.seed(args["seed"])
+    if seed is not None:
+        random.seed(str(seed))
 
     # Set up and run the simulation
     env = simpy.Environment()
@@ -29,58 +50,33 @@ def simulate():
     global_logger_memory_handler = GlobalLoggerMemoryHandler()
     logging_level = logging.INFO
 
-    try:
-        gateway = Gateway(
+    gateway = Gateway(
+        env,
+        data_channels,
+        data_slots_per_channel,
+        request_slots,
+        rrm_period,
+        max_cycles,
+        global_logger_memory_handler,
+        logging_level,
+        stat_tracker,
+    )
+    sensors = [
+        Sensor(
             env,
-            args["data_channels"],
-            args["data_slots_per_channel"],
-            args["request_slots"],
-            args["rrm_period"],
-            args["max_cycles"],
+            i,
+            sensors_measurement_chance,
+            gateway.rrm_message_event,
+            gateway.transmission_request_messages,
+            gateway.sensor_data_messages,
             global_logger_memory_handler,
             logging_level,
             stat_tracker,
         )
-        sensors = [
-            Sensor(
-                env,
-                i,
-                args["sensors_measurement_chance"],
-                gateway.rrm_message_event,
-                gateway.transmission_request_messages,
-                gateway.sensor_data_messages,
-                global_logger_memory_handler,
-                logging_level,
-                stat_tracker,
-            )
-            for i in range(args["sensor_count"])
-        ]
-    except ValueError as e:
-        return str(e), HTTPStatus.BAD_REQUEST
+        for i in range(sensor_count)
+    ]
 
-
-    env.run()
-
-    # Prepare response
-    response = {
-        "log": global_logger_memory_handler.log,
-        "ftr_values": stat_tracker.ftr_tracker,
-        "measurement_latencies": stat_tracker.measurement_latencies
-    }
-
-    return jsonify(response)
-
-
-def parse_query_params():
-    args = {}
-    for key, default_value in DEFAULT_CONFIG.items():
-        if isinstance(default_value, bool):
-            args[key] = request.args.get(key, default=default_value, type=lambda v: v.lower() == 'true')
-        elif isinstance(default_value, (int, float)):
-            args[key] = request.args.get(key, default=default_value, type=type(default_value))
-        else:
-            args[key] = request.args.get(key, default=default_value)
-    return args
+    return env, stat_tracker, global_logger_memory_handler, gateway, sensors
 
 
 if __name__ == "__main__":
