f2bb9067a4e8f6aed58c71a4364d5b246227fa5b
EdoardoLuciani <edoardo.luciani@gmail.com>
2024-11-20 11:32:04 +0000

    Add more matches

--- frontend/src/js/helpers/log-matcher-helper.js
+++ frontend/src/js/helpers/log-matcher-helper.js
@@ -11,3 +11,16 @@ export function matches_finished_request_reply_message(line) {
     STANDARD_PATTERN + "Finished RequestReplyMessage transmission",
   );
 }
+
+export function matches_started_transmission_request_message(line) {
+  return line.match(
+    STANDARD_PATTERN +
+      "Started TransmissionRequestMessage for request slot ([0-9]*) transmission",
+  );
+}
+
+export function matches_started_sensor_measurement_message(line) {
+  return line.match(
+    STANDARD_PATTERN + "Started SensorMeasurementMessage transmission",
+  );
+}
