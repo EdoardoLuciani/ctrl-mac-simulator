9321c4d37855d48a75d5735352d8d165381ed14a
EdoardoLuciani <edoardo.luciani@gmail.com>
2024-11-26 13:16:12 +0000

    Test seeds in different format

--- backend/tests/test_integration.py
+++ backend/tests/test_integration.py
@@ -6,8 +6,8 @@ from simulation.stat_tracker import StatTracker
 import random, simpy
 
 
-def test_scenario_1():
-    env, stat_tracker, global_logger_memory_handler, gateway, sensors, seed = setup_simulation(3, 2, 6, 0.5, 6, 6, seed=834)
+def test_scenario_random_seed():
+    env, stat_tracker, global_logger_memory_handler, gateway, sensors, seed = setup_simulation(3, 2, 6, 0.5, 6, 6, seed="123")
 
     env.run(0.50)
     # RRM message in first cycle must have all slots free and sensors are ready to send the transmission request on the next cycle
@@ -21,30 +21,33 @@ def test_scenario_1():
 
     env.run(1.5)
     # Two sets of slots collide and receive different backoff time, some other proceed with data transmission and go idle
-    assert [slot.state for slot in gateway._rrm.request_slots] == [
-        "contention",
-        "free",
-        "no_contention",
-        "no_contention",
-        "free",
-        "contention",
-    ]
+    assert [slot.state for slot in gateway._rrm.request_slots] == ['free', 'no_contention', 'no_contention', 'no_contention', 'contention', 'no_contention']
 
     wanted_states = [
-        _DataTransmissionState,  # 0 period backoff, so sent another transmission request
+        _IdleState,  # 0 period backoff, so sent another transmission request
         _IdleState,
-        _TransmissionRequestState,  # backed off for 1 period, awaiting for its turn
-        _TransmissionRequestState,  # backed off for 1 period, awaiting for its turn
-        _DataTransmissionState,  # 0 period backoff, so sent another transmission request
+        _DataTransmissionState,  # backed off for 1 period, awaiting for its turn
+        _DataTransmissionState,  # backed off for 1 period, awaiting for its turn
+        _IdleState,  # 0 period backoff, so sent another transmission request
         _IdleState,
     ]
+
     assert all([isinstance(sensor._state, cls) for sensor, cls in zip(sensors, wanted_states)])
 
     assert len(stat_tracker.ftr_values) != 0
     assert len(stat_tracker.measurement_latencies) != 0
 
+def test_seed_converted_to_string():
+    env, _, glmh, _, _, seed = setup_simulation(3, 2, 6, 0.5, 6, 6, seed="123")
+    env2, _, glmh2, _, _, seed2 = setup_simulation(3, 2, 6, 0.5, 6, 6, seed=123)
 
-def test_scenario_2():
+    env.run()
+    env2.run()
+    assert glmh.log == glmh2.log
+    assert seed == seed2
+
+
+def test_scenario_lucky_seed_no_collision():
     env, stat_tracker, global_logger_memory_handler, gateway, sensors, seed = setup_simulation(3, 2, 6, 0.5, 2, 6, seed = "226")
 
     env.run()
