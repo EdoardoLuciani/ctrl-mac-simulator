f3c408153d7c16c9a1cf5bfb2d4e85fa55ef05b2
EdoardoLuciani <edoardo.luciani@gmail.com>
2025-01-11 23:35:36 +0000

    Remove random usage for creating dummy bytes

--- backend/src/simulation/messages/sensor_measurement_message.py
+++ backend/src/simulation/messages/sensor_measurement_message.py
@@ -5,14 +5,14 @@ from simulation.messages.abstract_message import AbstractMessage
 
 class SensorMeasurementMessage(AbstractMessage):
     def __init__(self, sensor_id: int, data_channel: int, start_time: float, message_length: int):
-        self.data = random.randbytes(message_length)
+        self.message_length = message_length
         self.sensor_id = sensor_id
         self.data_channel = data_channel
         self.start_time = start_time
         self.arrive_time = start_time + self.get_airtime()
 
     def get_message_len(self) -> int:
-        return len(self.data)
+        return self.message_length
 
     def __str__(self) -> str:
         return self.__class__.__name__
