e873486cc8a8c4a7fe9291e8d381508889916f40
EdoardoLuciani <edoardo.luciani@gmail.com>
2025-01-11 18:51:17 +0000

    Fix airtime test

--- backend/tests/test_airtime.py
+++ backend/tests/test_airtime.py
@@ -4,6 +4,6 @@ import pytest
 
 def test_airtime():
     # Reference value computed by https://www.loratools.nl/#/airtime
-    sensor_message = SensorMeasurementMessage(0, 0, 12)
+    sensor_message = SensorMeasurementMessage(0, 0, 12, 12)
 
     assert sensor_message.get_airtime() == pytest.approx(0.04122, 1e-4)
